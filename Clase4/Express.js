52c8fadd1884db44277fe39cd01eef8aef717ed9
Breinner09 <breinnerpg09@gmail.com>
2023-02-24 18:16:11 -0500

    Clase4

--- Clase4/Express.js
+++ Clase4/Express.js
@@ -1,68 +1,80 @@
+const { json } = require('express');
 const express = require('express');
+const fs = require('fs')
 const app = express();
 const host = "localhost";
-const port = "5355";
+const port = 3000;
 
-//para leer json
-// app.use(express.json())
 
-//para leer tipo texto 
-// app.use(express.text())
+//con este es para poder leer json
+//app.use(express.json())
 
-//para recibir formularios
-app.use(express.urlencoded());
+//este es para poder leer texto
+//app.use(express.text())
+app.use(express.urlencoded())//para recibir formularios
 
-app.get('/adios', (req, res) => {
+
+
+//app.get('/home',(req, res)=>{
+ //   res.send("<h1>Está en el Home</h1>")
+//})
+
+
+app.get('/persona/:name/:apellido',(req, res)=>{
+    let nombre=req.params.name
+    res.send(`Hola tu nombre es ${nombre} y tu apellido es ${req.params.apellido} `)
+})
+
+
+app.get('/home/query',(req, res)=>{
+    console.log(req.query);
+    res.send(`esta son las querys y la query es ${req.query.saludo}`)
+})
+
+app.get('/home/body',(req, res)=>{
     console.log(req.body.nombre);
+    res.send(req.body.nombre);
+    //console.log(req.body.nombre);
+    //console.log(`esto es desde el body ${req.body.nombre} ${req.body.apellido}`);
+    //res.send(`esta son las querys y la query es ${req.body.nombre}  ${req.body.apellido}`)
+    //res.json(req.body) asi se llama el obj completo
+
+})
+
+app.get('/adios',(req, res)=>{
+    console.log(req.body.name);
+    res.send(`Buena suerte ${req.body.name}`)
 })
 
 
-// app.get('/home', (req, res) => {
-//     res.send("hola desde home")
-// });
-
-// app.get('/persona/:name', (req, res) => {
-//     let name = req.params.name;
-//     res.send(`Hola ${name}`)
-// });
-
-// app.get('/home/query', (req, res) => {
-//     console.log(req.query);
-//     res.send(`la query es ${req.query.saludo}`)
-// });
-
-// app.get('/home/body', (req, res) => {
-//     const constBody = req.body;
-//     // res.send(`el nombre que contiene el body es ${constBody}`)
-//     // res.json(constBody);
-//     res.send(req.body);
-// });
 
 
 
-// app.post('/help', (req, res) => {
-//     res.send("hola desde help")
-// });
 
-app.use((req, res, next) => {
-    if (req.query.nombre === "brei") {
-        next()
-    } else {
-        res.send("Error, no hay query")
+/*
+app.post('/help',(req, res)=>{
+    res.send("<h1>Está en el help</h1>")
+})
+
+*/
+app.use((req, res,next)=>{
+    if (req.query.nombre === "julian") {
+        next();
+    }else{
+        res.send("ERROR NO HAY QUERY")
     }
-
 })
 
-app.get("/home", (req, res) => {
-    res.send(`la query es ${req.query.nombre}`)
-});
+app.get("/home",(req, res) =>{
+    res.send(`La query es ${req.query.nombre}`)
+})
 
-app.use((req, res) => {
-    res.status(404).send("<h1 style= color:red>Error 404<h1>");
-});
+app.use((req, res)=>{
+    res.status(404).send("<h1>Error 404 </h1>")
+})
 
-app.listen(port, host, () => {
-    console.log(`el link de acc es http://${host}:${port} `);
-});
+app.listen(port,host,()=>{
+    console.log(`el link de acc es http://${host}:${port}`);
+})
 
 
